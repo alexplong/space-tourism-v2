1c8aeaaef4327bb5e81e9cc0412fabd27e9b72a1
ALX <3496916+alexplong@users.noreply.github.com>
2022-04-12 09:55:47 -0700

    First commit

---
+++ nav.js
@@ -0,0 +1,14 @@
+/** @format */
+
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+  } else {
+    primaryNav.setAttribute("data-visible", false);
+  }
+});
