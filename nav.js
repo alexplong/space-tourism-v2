b13c3e032983895e11efd9de12116ed194d8903a
Alex Plong <alexplong@gmail.com>
2022-04-18 11:14:56 -0700

    - Mobile homepage close - Tablet and Desktop homepage next

--- nav.js
+++ nav.js
@@ -8,7 +8,9 @@ navToggle.addEventListener("click", () => {
 
   if (visibility === "false") {
     primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
   } else {
     primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
   }
 });
